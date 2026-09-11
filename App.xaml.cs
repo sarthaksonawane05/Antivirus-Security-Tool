9444889e5475af8c56d69eaaefea703ff3f9bbd0
sarthaksonawane05 <sonawanesarthak05@gmail.com>
2026-09-11 18:37:58 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Antivirus_and_security
+{
+    public partial class App : Application
+    {
+        public void ChangeTheme(string themeName)
+        {
+            ResourceDictionary theme = new ResourceDictionary();
+
+            switch (themeName)
+            {
+                case "Light":
+                    theme.Source = new Uri("Themes/Light_Theme.xaml", UriKind.Relative);
+                    break;
+
+                default:
+                    theme.Source = new Uri("Themes/Dark_Themes.xaml", UriKind.Relative);
+                    break;
+            }
+
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(theme);
+        }
+    }
+}
