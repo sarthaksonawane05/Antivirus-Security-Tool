31dd4073e8a0f27a37840a15061985f2583ea5a2
sarthaksonawane05 <sonawanesarthak05@gmail.com>
2026-09-11 20:03:01 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using Antivirus_and_security.View;
+
+namespace Antivirus_and_security
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainContent.Content = new Dashboard();
+        }
+
+        private void DashboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Dashboard();
+        }
+
+        private void ProtectionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Protection();
+        }
+
+        private void PrivacyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Privacy();
+        }
+
+        private void PerformanceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Performance();
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Settings();
+        }
+
+        // =====================================================
+        // LIGHT THEME
+        // =====================================================
+
+        private void LightTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).ChangeTheme("Light");
+        }
+
+        // =====================================================
+        // DARK THEME
+        // =====================================================
+
+        private void DarkTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).ChangeTheme("Dark");
+        }
+    }
+}
